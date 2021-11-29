a745d6426a51266f96c211f7a69599243b7d5c0f
GOUSSANOU Lauryn Mary <laryGOUSS@gmail.com>
2021-11-29 12:35:03 +0100

    création de l'activité

---
+++ app/src/main/java/marylary/demo/CalculatriceActivity.java
@@ -0,0 +1,24 @@
+package marylary.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice2);
+    }
+
+    public void opperationEvent(View view) {
+    }
+
+    public void buttonEvent(View view) {
+    }
+
+    public void equalsEvent(View view) {
+    }
+}
