a75ff926f824aa8c49526a5e3d68efdbbf5e15e3
GOUSSANOU Lauryn Mary <laryGOUSS@gmail.com>
2021-12-12 16:11:47 +0100

    Mise à jour

--- app/src/main/java/marylary/demo/Calculate.java
+++ app/src/main/java/marylary/demo/Calculate.java
@@ -1,4 +1,68 @@
 package marylary.demo;
 
 public class Calculate {
+
+    private double result =0;
+    public Calculate effacer(){
+
+        this.result = 0;
+        return this;
+    }
+
+
+    public Calculate operation(double a, double b){
+        this.result = this.result + a + b;
+
+        return  this;
+    }
+
+    public  Calculate multiplication(double a, double b){
+
+        if (this.result ==0){
+            this.result = a * b;
+            return  this;
+
+        }
+        this.result = this.result * a * b;
+        return this;
+    }
+
+
+
+    public Calculate division(double a, double b){
+        if(this.result == 0 && b != 0){
+            this.result = a / b;
+            return this;
+        }
+        if(a != 0 && b != 0){
+            this.result = this.result / a / b;
+            return this;
+        }
+        return this;
+    }
+    public Calculate soustration(double a, double b){
+        this.result = this.result - a - b;
+        return this;
+    }
+    public Calculate reste(double a, double b){
+        if(this.result == 0 && b != 0){
+            this.result = a % b;
+            return this;
+        }
+        if(a != 0 && b != 0){
+            this.result = this.result % a % b;
+            return this;
+        }
+        return this;
+    }
+
+
+    public  int toInt(){
+        return  (int) this.result;
+    }
+
+    //autre methode
+    public  double toDouble(){
+        return (double) this.result;
+    }
 }
